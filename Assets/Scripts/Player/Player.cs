425d675a5e0fa063b0931758ef57a0c9172bf806
James <127003545+hawkinsjch@users.noreply.github.com>
2024-03-07 00:32:09 -0600

    Basic Left/Right Walk
    Edited Player.cs in order to allow for basic horizontal walking. This method may lead to issue with grappling since I am setting the RigidBody Velocity X, so it will need modification.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Min(0)]
+    public float walkSpeed;
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float movement = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movement * walkSpeed, rb.velocity.y);
     }
 }

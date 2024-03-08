a542f151e6edfbf69a653c90cc3f4096114ae98a
James <127003545+hawkinsjch@users.noreply.github.com>
2024-03-08 16:29:12 -0600

    Added Grapple Acceleration + Velocity Clamps

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -10,8 +10,11 @@ public class Player : MonoBehaviour
     public LayerMask layerMask;
     public Rigidbody2D rb;
 
-    public float grappleVelocity = 1;
+    public float grappleInitVelocity = 1;
+    public float grappleAcceleration = 1;
+    public float grappleMaxVelocity = 1;
     private bool grappled = false;
+    private float grappledTime = 0;
     
     public float maxGroundDistance = 0.1f;
 
@@ -26,6 +29,7 @@ public class Player : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(transform.position, (mousePos - (Vector2)transform.position).normalized, Mathf.Infinity, layerMask);
         if (hit)
         {
+            grappledTime = 0;
             grappled = true;
             lastPlayerPos = transform.position;
             lastHitPoint = hit.point;
@@ -49,20 +53,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Physics2D.Raycast(transform.position, Vector2.down, maxGroundDistance + (transform.localScale.y / 2), layerMask))
         {
             float walkDir = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(walkDir * walkSpeed, rb.velocity.y);
         }
+        
 
         if (grappled)
         {
-            rb.velocity = ((lastHitPoint - (Vector2)transform.position).normalized * grappleVelocity);
+            rb.velocity = ((lastHitPoint - (Vector2)transform.position).normalized * Mathf.Clamp(grappleInitVelocity + grappleAcceleration * grappledTime, grappleInitVelocity, grappleMaxVelocity));
             rb.gravityScale = 0;
+            grappledTime += Time.deltaTime;
         }
         else
         {
-            rb.gravityScale = 1;
+            rb.gravityScale = 6;
         }
 
         if (Input.GetMouseButtonDown(0)) {
@@ -70,10 +77,15 @@ public class Player : MonoBehaviour
             {
                 Grapple();
             }
-            else
-            {
-                grappled = false;
-            }
+        }
+        
+        if (Input.GetMouseButton(0) && grappled)
+        {
+
+        }
+        else
+        {
+            grappled = false;
         }
     }
 }

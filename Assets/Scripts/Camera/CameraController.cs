ec8fce5800596007b9c58e7f6fd411ff03b5b0b7
James <127003545+hawkinsjch@users.noreply.github.com>
2024-03-11 23:49:01 -0500

    Added Camera Transition

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField]
+    [Min(0)]
+    private float transitionTime;
+
+    private Vector2 lastRoomPos;
+    private float currentTransitionTime;
+
     private Transform player;
 
     private Vector2 upperBound;
@@ -13,8 +20,21 @@ public class CameraController : MonoBehaviour
 
     public void ChangeRoom(Room _room)
     {
-        room = _room;
-        (lowerBound, upperBound) = room.GetBounds();
+        if (_room != room)
+        {
+            if (room)
+            {
+                lastRoomPos = transform.position;
+                currentTransitionTime = 0;
+            }
+            else
+            {
+                currentTransitionTime = transitionTime;
+            }
+
+            room = _room;
+            (lowerBound, upperBound) = room.GetBounds();
+        }
     }
 
     private void Awake()
@@ -27,6 +47,13 @@ public class CameraController : MonoBehaviour
     {
         float x = Mathf.Clamp(player.position.x, lowerBound.x, upperBound.x);
         float y = Mathf.Clamp(player.position.y, lowerBound.y, upperBound.y);
+        if (currentTransitionTime < transitionTime)
+        {
+            currentTransitionTime += Time.deltaTime;
+            float transPerc = Mathf.Clamp(currentTransitionTime / transitionTime, 0, 1);
+            x = Mathf.Lerp(lastRoomPos.x, x, transPerc);
+            y = Mathf.Lerp(lastRoomPos.y, y, transPerc);
+        }
 
         gameObject.transform.position = new Vector3(x, y, -10);
     }

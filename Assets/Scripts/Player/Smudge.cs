4425cfc0ce9aaa89bbdfca180692b559529db892
James <127003545+hawkinsjch@users.noreply.github.com>
2024-03-16 21:11:50 -0500

    Smudge Fade

---
+++ Assets/Scripts/Player/Smudge.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smudge : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector2 lifeTimeRange;
+    private float lifeTime;
+
+    private float time;
+
+    private SpriteRenderer sR;
+
+    private void Awake()
+    {
+        lifeTime = Random.Range(lifeTimeRange.x, lifeTimeRange.y);
+        sR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        float lifePerc = time / lifeTime;
+        sR.color = new Color(1, 1, 1, 1 - lifePerc);
+        if (time >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
